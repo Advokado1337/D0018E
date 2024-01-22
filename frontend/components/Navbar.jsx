a61880e348fdc0b78f27a28fdf9021297b57ca6c
t0sic <kanieckimikolaj@gmail.com>
2024-01-22 15:23:28 +0100

    Github account issue

--- frontend/components/Navbar.jsx
+++ frontend/components/Navbar.jsx
@@ -21,7 +21,6 @@ const Navbar = () => {
                             <img
                                 className="h-8 w-auto"
                                 src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
-                                alt="Your Company"
                             />
                         </div>
                         <div className="hidden sm:ml-6 sm:block">
