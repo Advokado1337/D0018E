01e0e5b52cace6736d53073857bbf39cc6eb4fc8
t0sic <kanieckimikolaj@gmail.com>
2024-01-30 14:49:29 +0100

    Fixed so api route for product only returns one object

--- backend/routes/product.js
+++ backend/routes/product.js
@@ -40,6 +40,7 @@ export default {
             `SELECT * FROM product WHERE product_id = ${params.id}`,
             (err, result) => {
                 if (err) return res.sendStatus(500)
+                if (result.length) return res.send(result[0])
                 res.send(result)
             }
         )
