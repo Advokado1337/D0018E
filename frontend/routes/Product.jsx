14868dabb5f5289327752c21690b4ba5370b2f56
t0sic <kanieckimikolaj@gmail.com>
2024-02-19 14:08:47 +0100

    Added reviews on product page

--- frontend/routes/Product.jsx
+++ frontend/routes/Product.jsx
@@ -9,6 +9,25 @@ const Product = () => {
     const [selectedSize, setSelectedSize] = useState()
     const [addToCartThrottle, setAddToCartThrottle] = useState(false)
     const [inStock, setInStock] = useState(false)
+    const [reviewModal, setReviewModal] = useState({
+        open: false,
+        text: "",
+        rating: 0,
+    })
+    const [reviews, setReviews] = useState([
+        {
+            name: "John Doe",
+            rating: 4,
+            text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+            createdAt: "2021-10-10",
+        },
+        {
+            name: "John Doe",
+            rating: 2,
+            text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+            createdAt: "2021-10-10",
+        },
+    ])
     const navigate = useNavigate()
 
     useEffect(() => {
@@ -69,6 +88,10 @@ const Product = () => {
         },
     }
 
+    const handleReviewModalOpen = () => {
+        setReviewModal({ open: true, text: "", rating: 0 })
+    }
+
     const handleAddToCart = () => {
         if (addToCartThrottle || !inStock) return
         setAddToCartThrottle(true)
@@ -91,82 +114,231 @@ const Product = () => {
     if (!product) return <div>Loading...</div>
 
     return (
-        <div className="flex py-8">
-            <div className="flex w-full">
-                <div className="w-full border rounded-md bg-gray aspect-square overflow-hidden">
-                    <img
-                        src={"/images/products/" + product.image}
-                        alt={product.label}
-                        className="object-cover w-full h-full"
-                    />
-                </div>
-                <div className="w-full flex-col flex px-4">
-                    <h2 className="font-semibold text-3xl capitalize">
-                        {product.label}
-                    </h2>
-                    <h2 className="text-3xl mt-2">${product.price}</h2>
-                    <h4 className="mt-4">Color</h4>
-                    <div className="mt-2">
-                        {product.colors.map((color) => (
-                            <div
-                                className="inline-block mr-2 cursor-pointer hover:opacity-80"
-                                key={color}
-                            >
-                                <div
-                                    onClick={() => setSelectedColor(color)}
+        <div className="flex flex-col py-8">
+            {reviewModal.open ? (
+                <div className="w-full h-full items-center justify-center flex">
+                    <div className="flex flex-col min-w-[700px]">
+                        <h1 className="font-semibold text-3xl">
+                            Write a Review
+                        </h1>
+                        <p className="text-gray-600">
+                            Please leave a review to help others. Thank you!
+                        </p>
+                        <div className="flex mt-4 mx-auto">
+                            {Array.from({ length: 5 }).map((_, i) => (
+                                <svg
+                                    onClick={() =>
+                                        setReviewModal({
+                                            ...reviewModal,
+                                            rating: i + 1,
+                                        })
+                                    }
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    viewBox="0 0 24 24"
                                     className={
-                                        "w-10 h-10 flex justify-center items-center rounded-full " +
-                                        (color === selectedColor
-                                            ? colors[color].border + " border-2"
-                                            : "")
+                                        (i < reviewModal.rating
+                                            ? "fill-yellow-400 stroke-yellow-400"
+                                            : "stroke-gray-500 fill-gray-500") +
+                                        " w-12 h-12"
                                     }
                                 >
+                                    <path
+                                        strokeLinecap="round"
+                                        strokeLinejoin="round"
+                                        d="M11.48 3.499a.562.562 0 0 1 1.04 0l2.125 5.111a.563.563 0 0 0 .475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 0 0-.182.557l1.285 5.385a.562.562 0 0 1-.84.61l-4.725-2.885a.562.562 0 0 0-.586 0L6.982 20.54a.562.562 0 0 1-.84-.61l1.285-5.386a.562.562 0 0 0-.182-.557l-4.204-3.602a.562.562 0 0 1 .321-.988l5.518-.442a.563.563 0 0 0 .475-.345L11.48 3.5Z"
+                                    />
+                                </svg>
+                            ))}
+                        </div>
+                        <label className="font-medium mt-4">
+                            Product Review
+                        </label>
+                        <textarea
+                            onChange={(e) => {
+                                setReviewModal({
+                                    ...reviewModal,
+                                    text: e.target.value,
+                                })
+                            }}
+                            className="mt-2 p-4 border rounded-md w-full h-48 outline-none focus:ring-indigo-500 ring-2"
+                        >
+                            {reviewModal.text}
+                        </textarea>
+                        <div className="flex flex-row-reverse">
+                            <button className="mt-4 p-4 rounded-md text-white justify-center flex bg-indigo-500 hover:bg-indigo-600">
+                                Submit
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            ) : (
+                <>
+                    <div className="flex w-full">
+                        <div className="w-full border rounded-md bg-gray aspect-square overflow-hidden">
+                            <img
+                                src={"/images/products/" + product.image}
+                                alt={product.label}
+                                className="object-cover w-full h-full"
+                            />
+                        </div>
+                        <div className="w-full flex-col flex px-4">
+                            <h2 className="font-semibold text-3xl capitalize">
+                                {product.label}
+                            </h2>
+                            <h2 className="text-3xl mt-2">${product.price}</h2>
+                            <h4 className="mt-4">Color</h4>
+                            <div className="mt-2">
+                                {product.colors.map((color) => (
                                     <div
+                                        className="inline-block mr-2 cursor-pointer hover:opacity-80"
+                                        key={color}
+                                    >
+                                        <div
+                                            onClick={() =>
+                                                setSelectedColor(color)
+                                            }
+                                            className={
+                                                "w-10 h-10 flex justify-center items-center rounded-full " +
+                                                (color === selectedColor
+                                                    ? colors[color].border +
+                                                      " border-2"
+                                                    : "")
+                                            }
+                                        >
+                                            <div
+                                                className={
+                                                    "w-8 h-8 rounded-full " +
+                                                    colors[color].bg +
+                                                    (color === "white"
+                                                        ? " border border-gray-400"
+                                                        : "")
+                                                }
+                                            ></div>
+                                        </div>
+                                    </div>
+                                ))}
+                            </div>
+                            <h4 className="mt-4">Size</h4>
+                            <div className="flex mt-2">
+                                {product.sizes.map((size) => (
+                                    <div
+                                        key={size}
+                                        onClick={() => setSelectedSize(size)}
                                         className={
-                                            "w-8 h-8 rounded-full " +
-                                            colors[color].bg +
-                                            (color === "white"
-                                                ? " border border-gray-400"
+                                            "border cursor-pointer rounded-md py-3 px-6 border-gray-400 mr-2 hover:bg-indigo-500 hover:text-white hover:border-indigo-500 " +
+                                            (selectedSize === size
+                                                ? "bg-indigo-500 text-white border-indigo-500"
                                                 : "")
                                         }
-                                    ></div>
-                                </div>
+                                    >
+                                        {size}
+                                    </div>
+                                ))}
                             </div>
-                        ))}
-                    </div>
-                    <h4 className="mt-4">Size</h4>
-                    <div className="flex mt-2">
-                        {product.sizes.map((size) => (
-                            <div
-                                key={size}
-                                onClick={() => setSelectedSize(size)}
+                            <h4 className="mt-4">Description</h4>
+                            <p className="mt-2 text-gray-700">
+                                {product.description}
+                            </p>
+                            <button
+                                onClick={handleAddToCart}
+                                title={inStock ? "Add to cart" : "Out of stock"}
                                 className={
-                                    "border cursor-pointer rounded-md py-3 px-6 border-gray-400 mr-2 hover:bg-indigo-500 hover:text-white hover:border-indigo-500 " +
-                                    (selectedSize === size
-                                        ? "bg-indigo-500 text-white border-indigo-500"
-                                        : "")
+                                    "mt-4 py-3 rounded-md text-white justify-center flex w-full" +
+                                    (addToCartThrottle || !inStock
+                                        ? " cursor-not-allowed bg-gray-400"
+                                        : " bg-indigo-500 hover:bg-indigo-600")
                                 }
                             >
-                                {size}
-                            </div>
-                        ))}
+                                Add to cart
+                            </button>
+                        </div>
                     </div>
-                    <h4 className="mt-4">Description</h4>
-                    <p className="mt-2 text-gray-700">{product.description}</p>
-                    <button
-                        onClick={handleAddToCart}
-                        title={inStock ? "Add to cart" : "Out of stock"}
-                        className={
-                            "mt-4 py-3 rounded-md text-white justify-center flex w-full" +
-                            (addToCartThrottle || !inStock
-                                ? " cursor-not-allowed bg-gray-400"
-                                : " bg-indigo-500 hover:bg-indigo-600")
-                        }
-                    >
-                        Add to cart
-                    </button>
-                </div>
-            </div>
+                    <div className="w-full flex flex-col mt-8">
+                        <div className="flex items-center border-b border-gray-200 pb-8">
+                            <div className="flex flex-col">
+                                <h2 className="font-medium text-2xl">
+                                    Reviews
+                                </h2>
+                                <p className="mt-1 text-gray-600">
+                                    If you have purchased this product, please
+                                    leave a review to help others. Thank you!
+                                </p>
+                            </div>
+                            <button
+                                className="ml-auto bg-indigo-500 text-white px-4 py-4 rounded-md hover:bg-indigo-600 active:bg-indigo-700"
+                                onClick={handleReviewModalOpen}
+                            >
+                                Write a Review
+                            </button>
+                        </div>
+                        {reviews.length ? (
+                            <div className="flex flex-col items-center">
+                                {reviews.map((review) => (
+                                    <div className="flex w-full max-w-[700px] mt-8">
+                                        <div className="bg-gray-400 shrink-0 rounded-full h-12 w-12 flex items-center justify-center">
+                                            <svg
+                                                xmlns="http://www.w3.org/2000/svg"
+                                                fill="none"
+                                                viewBox="0 0 24 24"
+                                                strokeWidth={1.5}
+                                                stroke="white"
+                                                className="w-8 h-8"
+                                            >
+                                                <path
+                                                    strokeLinecap="round"
+                                                    strokeLinejoin="round"
+                                                    d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z"
+                                                />
+                                            </svg>
+                                        </div>
+                                        <div className="ml-4 w-full flex flex-col border-b pb-8 border-gray-300">
+                                            <div className="font-medium">
+                                                {review.name}
+                                            </div>
+                                            <div className="text-gray-600">
+                                                {new Date(
+                                                    review.createdAt
+                                                ).toDateString()}
+                                            </div>
+                                            <div className="flex my-2">
+                                                {Array.from({ length: 5 }).map(
+                                                    (_, i) => (
+                                                        <svg
+                                                            xmlns="http://www.w3.org/2000/svg"
+                                                            viewBox="0 0 24 24"
+                                                            strokeWidth={1.5}
+                                                            className={
+                                                                (i <
+                                                                review.rating
+                                                                    ? "fill-yellow-400 stroke-yellow-400"
+                                                                    : "stroke-gray-500 fill-gray-500") +
+                                                                " w-6 h-6"
+                                                            }
+                                                        >
+                                                            <path
+                                                                strokeLinecap="round"
+                                                                strokeLinejoin="round"
+                                                                d="M11.48 3.499a.562.562 0 0 1 1.04 0l2.125 5.111a.563.563 0 0 0 .475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 0 0-.182.557l1.285 5.385a.562.562 0 0 1-.84.61l-4.725-2.885a.562.562 0 0 0-.586 0L6.982 20.54a.562.562 0 0 1-.84-.61l1.285-5.386a.562.562 0 0 0-.182-.557l-4.204-3.602a.562.562 0 0 1 .321-.988l5.518-.442a.563.563 0 0 0 .475-.345L11.48 3.5Z"
+                                                            />
+                                                        </svg>
+                                                    )
+                                                )}
+                                            </div>
+                                            <div className="mt-2">
+                                                {review.text}
+                                            </div>
+                                        </div>
+                                    </div>
+                                ))}
+                            </div>
+                        ) : (
+                            <div className="flex items-center justify-center py-12">
+                                No reviews yet
+                            </div>
+                        )}
+                    </div>
+                </>
+            )}
         </div>
     )
 }
