df18eb2175d656813a03f6ea73a6c3b2ea2d5215
unknown <santib-0@student.ltu.se>
2024-02-21 15:21:45 +0100

    Removed image upload option when creating

--- frontend/components/ProductEditor.jsx
+++ frontend/components/ProductEditor.jsx
@@ -93,82 +93,87 @@ const ProductEditor = ({
                             </button>
                         </div>
                     </div>
-                    <div className="bg-gray-300 rounded-md p-4 mt-4">
-                        <h3 className="font-semibold text-gray-500">Images</h3>
-                        <div className="py-2 flex overflow-x-auto flex-nowrap">
-                            {product.images.map((image, i) => (
-                                <div
-                                    key={i}
-                                    className="h-[300px] w-[300px] bg-cover bg-no-repeat overflow-hidden rounded-md mr-4 relative shrink-0"
-                                    style={{
-                                        backgroundImage:
-                                            "url(" +
-                                            (image.url
-                                                ? "/images/products/" +
-                                                  image.url
-                                                : URL.createObjectURL(
-                                                      image.file
-                                                  )) +
-                                            ")",
-                                    }}
-                                >
-                                    <div className="m-2 flex">
-                                        <div className="w-10 h-10 flex justify-center items-center rounded-full ">
-                                            <div
-                                                className={
-                                                    "w-8 h-8 rounded-full " +
-                                                    colors[image.color].bg
-                                                }
-                                            ></div>
-                                        </div>
-                                        <button
-                                            onClick={() =>
-                                                handleImageRemove(image)
-                                            }
-                                            className="w-10 h-10 flex items-center justify-center bg-red-500 rounded-md ml-auto"
-                                        >
-                                            <svg
-                                                xmlns="http://www.w3.org/2000/svg"
-                                                viewBox="0 0 24 24"
-                                                stroke-width="1.5"
-                                                class="w-6 h-6 stroke-white fill-none"
-                                            >
-                                                <path
-                                                    stroke-linecap="round"
-                                                    stroke-linejoin="round"
-                                                    d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"
-                                                />
-                                            </svg>
-                                        </button>
-                                    </div>
-                                </div>
-                            ))}
-                            {product.images.length < product.colors.length && (
-                                <button
-                                    onClick={onUploadButtonClick}
-                                    className="flex relative items-center justify-center bg-white rounded-md border border-dashed border-gray-400 h-[300px] w-[300px] shrink-0"
-                                >
-                                    <div className="font-semibold mb-2 text-xl">
-                                        Upload
-                                    </div>
-                                    <svg
-                                        xmlns="http://www.w3.org/2000/svg"
-                                        fill="none"
-                                        viewBox="0 0 24 24"
-                                        strokeWidth={1.5}
-                                        stroke="currentColor"
-                                        className="w-10 h-10"
+                    {editing && (
+                        <div className="bg-gray-300 rounded-md p-4 mt-4">
+                            <h3 className="font-semibold text-gray-500">
+                                Images
+                            </h3>
+                            <div className="py-2 flex overflow-x-auto flex-nowrap">
+                                {product.images.map((image, i) => (
+                                    <div
+                                        key={i}
+                                        className="h-[300px] w-[300px] bg-cover bg-no-repeat overflow-hidden rounded-md mr-4 relative shrink-0"
+                                        style={{
+                                            backgroundImage:
+                                                "url(" +
+                                                (image.url
+                                                    ? "/images/products/" +
+                                                      image.url
+                                                    : URL.createObjectURL(
+                                                          image.file
+                                                      )) +
+                                                ")",
+                                        }}
                                     >
-                                        <path
-                                            strokeLinecap="round"
-                                            strokeLinejoin="round"
-                                            d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5m-13.5-9L12 3m0 0 4.5 4.5M12 3v13.5"
-                                        />
-                                    </svg>
-                                </button>
-                            )}
+                                        <div className="m-2 flex">
+                                            <div className="w-10 h-10 flex justify-center items-center rounded-full ">
+                                                <div
+                                                    className={
+                                                        "w-8 h-8 rounded-full " +
+                                                        colors[image.color].bg
+                                                    }
+                                                ></div>
+                                            </div>
+                                            <button
+                                                onClick={() =>
+                                                    handleImageRemove(image)
+                                                }
+                                                className="w-10 h-10 flex items-center justify-center bg-red-500 rounded-md ml-auto"
+                                            >
+                                                <svg
+                                                    xmlns="http://www.w3.org/2000/svg"
+                                                    viewBox="0 0 24 24"
+                                                    stroke-width="1.5"
+                                                    class="w-6 h-6 stroke-white fill-none"
+                                                >
+                                                    <path
+                                                        stroke-linecap="round"
+                                                        stroke-linejoin="round"
+                                                        d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"
+                                                    />
+                                                </svg>
+                                            </button>
+                                        </div>
+                                    </div>
+                                ))}
+                                {product.images.length <
+                                    product.colors.length && (
+                                    <button
+                                        onClick={onUploadButtonClick}
+                                        className="flex relative items-center justify-center bg-white rounded-md border border-dashed border-gray-400 h-[300px] w-[300px] shrink-0"
+                                    >
+                                        <div className="font-semibold mb-2 text-xl">
+                                            Upload
+                                        </div>
+                                        <svg
+                                            xmlns="http://www.w3.org/2000/svg"
+                                            fill="none"
+                                            viewBox="0 0 24 24"
+                                            strokeWidth={1.5}
+                                            stroke="currentColor"
+                                            className="w-10 h-10"
+                                        >
+                                            <path
+                                                strokeLinecap="round"
+                                                strokeLinejoin="round"
+                                                d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5m-13.5-9L12 3m0 0 4.5 4.5M12 3v13.5"
+                                            />
+                                        </svg>
+                                    </button>
+                                )}
+                            </div>
                         </div>
-                    </div>
+                    )}
                     <div className="bg-gray-300 rounded-md p-4 mt-4">
                         <h3 className="font-semibold text-gray-500">Label</h3>
                         <input
